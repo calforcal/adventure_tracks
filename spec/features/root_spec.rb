91ff800811011eb568925fb182cfa6784c6fbd1f
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2023-12-13 19:42:00 -0700

    Add root_path test specs

---
+++ spec/features/root_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "root", type: :feature do
+  before(:each) do
+    visit root_path
+  end
+
+  describe "home page" do
+    describe "happys paths" do
+      it "can visit the home page" do
+        expect(page).to have_content("Adventure Tracks")
+      end
+
+      it "has a button to upload a file" do
+        expect(page).to have_content("Upload .gpx File")
+        click_button "Upload .gpx File"
+      end
+
+      it "takes you to a new page when the button is clicked" do
+        click_button "Upload .gpx File"
+        expect(current_path).to eq(maps_index_path)
+      end
+    end
+  end
+end
